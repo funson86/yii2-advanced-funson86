35c8a38434c0d0be3f54359f64ebf0115f2f3ff0
funson86 <funson86@gmail.com>
2015-01-05 16:08:45 +0800

    语言、时区、翻译文件

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,37 @@
 <?php
 return [
+    'name' => 'Advanced-funson86',
+    'language' => 'zh-CN',
+    'timeZone' => 'Asia/Shanghai',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    //'basePath' => '@app/messages',
+                    //'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                    ],
+                ],
+                /*'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'sourceLanguage' => 'zh-CN',
+                    'basePath' => '@app/messages'
+                ],*/
+            ],
+        ],
+        'formatter' => [
+            'dateFormat' => 'yyyy-MM-dd',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+            'decimalSeparator' => ',',
+            'thousandSeparator' => ' ',
+            'currencyCode' => 'CNY',
         ],
     ],
 ];
