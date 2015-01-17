a35295cf0325d05f0a5d209e3621c563ba387a01
funson86 <funson86@gmail.com>
2015-01-17 09:32:37 +0800

    add api of Restful

---
+++ api/config/main.php
@@ -0,0 +1,62 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),    
+    'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'basePath' => '@api/modules/v1',
+            'class' => 'api\modules\v1\Module'
+        ]
+    ],
+    'components' => [        
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule', 
+                    'controller' => 'v1/country',
+                    'tokens' => [
+                        '{id}' => '<id:\\w+>'
+                    ]
+                    
+                ],
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'v1/user',
+                    'tokens' => [
+                        '{id}' => '<id:\\w+>'
+                    ]
+
+                ],
+            ],
+        ]
+    ],
+    'params' => $params,
+];
+
+
+
